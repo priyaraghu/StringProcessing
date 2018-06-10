a4b2a1e3525c1617496598105164fb9bf8191e41
priyadarshini <priyadarshini.raghavendra@gmail.com>
2018-06-10 18:36:26 -0400

    changed the location of Dictionary file to Test package

--- Tests/test.py
+++ Tests/test.py
@@ -1,31 +1,31 @@
 from Compare.CompareFiles import compare
 
 try:
-    print (compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/dict.txt'))
+    print (compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/dict.txt'))
 except Exception as e:
     print('Test1 failed')
     print(e.args)
 
 try:
-    print (compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input1.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/dict.txt'))
+    print (compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input1.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/dict.txt'))
 except Exception as e:
     print('Test2 failed')
     print(e.args)
 
 try:
-    print (compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input2.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/dict.txt'))
+    print (compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input2.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/dict.txt'))
 except Exception as e:
     print('Test3 failed')
     print(e.args)
 
 try:
-    compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input5.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/dict.txt')
+    compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input5.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/dict.txt')
 except Exception as e:
     print('Test4 failed')
     print(e.args)
 
 try:
-    compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input6.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/dict.txt')
+    compare('/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/input6.txt','/Users/kavyadarshini/PycharmProjects/StringProcessing/Tests/dict.txt')
 except Exception as e:
     print('Test5 failed')
     print(e.args)
